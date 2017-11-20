35b7764a1ce5007f5a4133f1efd3d3db2620c043
liumapp <liumapp.com@gmail.com>
2017-11-20 09:49:55 +0800

    make it only for pattern

---
+++ src/main/java/com/liumapp/pattern/certificate/PersonalPattern.java
@@ -0,0 +1,12 @@
+package com.liumapp.pattern.certificate;
+
+/**
+ * Created by liumapp on 11/20/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class PersonalPattern {
+
+
+
+}
