74b0b1f680de64387a2035f1c8b715151168c80a
liumapp <liumapp.com@gmail.com>
2017-11-21 15:47:14 +0800

    add password check

--- src/main/java/com/liumapp/pattern/keystore/KeyStorePattern.java
+++ src/main/java/com/liumapp/pattern/keystore/KeyStorePattern.java
@@ -2,20 +2,23 @@ package com.liumapp.pattern.keystore;
 
 import com.liumapp.pattern.certificate.PersonalPattern;
 import com.liumapp.pattern.exception.PatternPropertiesNumberNotEnough;
+import com.liumapp.pattern.security.GeneratorPdn;
 
 /**
  * Created by liumapp on 11/21/17.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-public class KeyStorePattern {
+public class KeyStorePattern extends GeneratorPdn {
 
     private String name;
 
-    
+    private String keyStorePd;
+
+    private Integer keyLength;
 
     /**
-     * name_identityCode_sex_country_province_city
+     *
      * @param line
      */
     public static PersonalPattern parse(String line) throws PatternPropertiesNumberNotEnough {
---
+++ src/main/java/com/liumapp/pattern/security/GeneratorPdn.java
@@ -0,0 +1,20 @@
+package com.liumapp.pattern.security;
+
+/**
+ * generator password needed
+ * Created by liumapp on 11/21/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class GeneratorPdn {
+
+    public String generatorPd;
+
+    public String getGeneratorPd() {
+        return generatorPd;
+    }
+
+    public void setGeneratorPd(String generatorPd) {
+        this.generatorPd = generatorPd;
+    }
+}
