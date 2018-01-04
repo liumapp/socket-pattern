babaa7ecb7e4f0693d30a9328b71e68d36ec58c0
liumapp <liumapp.com@gmail.com>
2018-01-04 15:20:35 +0800

    add schedule hello pattern

---
+++ src/main/java/com/liumapp/pattern/schedule/HelloPattern.java
@@ -0,0 +1,9 @@
+package com.liumapp.pattern.schedule;
+
+/**
+ * Created by liumapp on 1/4/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class HelloPattern {
+}
