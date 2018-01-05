1f5b78761fcf07aff4369c017b5d899068d5919f
liumapp <liumapp.com@gmail.com>
2018-01-05 08:44:13 +0800

    add switch pattern

---
+++ src/main/java/com/liumapp/pattern/schedule/core/SwitchPattern.java
@@ -0,0 +1,24 @@
+package com.liumapp.pattern.schedule.core;
+
+import com.liumapp.pattern.encode.EncodingBack;
+
+/**
+ * Created by liumapp on 1/5/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class SwitchPattern implements EncodingBack {
+
+    private String type = "";
+
+    @Override
+    public String getEncoding() {
+        return null;
+    }
+
+    @Override
+    public boolean chk() {
+        return false;
+    }
+
+}
